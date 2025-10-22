f37353f207726867bf36bf4ad251cc364e5d6e06
3 <katherinerosariosison@gmail.com>
2025-10-23 05:57:18 +0800

    hi

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -1,15 +1,51 @@
 const dialogues = [
-  { 
-    bg: "bg.png",
-    png: "blank.png",
-    name: "Character Name",
-    dialogue: "Message."
-  },
-
-  { 
-    card: "card.png",
-    png: "blank.png",
-    name: "Character Name",
-    dialogue: "Message."
-  },
+{ 
+  bg: "bg.png",
+  png: "ibara-vs.png",
+  name: "Ibara",
+  dialogue: "Everyone! The broadcast of THUNDERBOLT CHALLENGE 4piece is finally approaching!"
+},
+{ 
+  bg: "bg.png",
+  png: "yuuta-vs.png",
+  name: "Yuuta",
+  dialogue: "What kind of auditions will take place at the fourth venue—the Arena, where we've been assigned?"
+},
+{ 
+  bg: "bg.png",
+  png: "yuuta-vs.png",
+  name: "Yuuta",
+  dialogue: "Just between us, doesn't the vice president have some kind of inside information?"
+},
+{ 
+  bg: "bg.png",
+  png: "ibara-vs.png",
+  name: "Ibara",
+  dialogue: "Unfortunately, I don’t know anything about the competition either. It'll be a fun, intense match once it starts!"
+},
+{ 
+  bg: "bg.png",
+  png: "yuuta-vs.png",
+  name: "Yuuta",
+  dialogue: "Well then, maybe I’ll surprise the vice president too."
+},
+{ 
+  bg: "bg.png",
+  png: "ibara-vs.png",
+  name: "Ibara",
+  dialogue: "That all depends on your skill. Of course, I have no intention of losing!"
+},
+{ 
+  bg: "bg.png",
+  png: "ibara-vs.png",
+  name: "Ibara",
+  dialogue: "That’s all from Ibara Saegusa of Eden!"
+},
+{ 
+  bg: "bg.png",
+  png: "yuuta-vs.png",
+  name: "Yuuta",
+  dialogue: "And Aoi Yuuta from 2wink! Don’t miss the show starting at 3 PM the day after tomorrow... ☆",
+  end: true
+},
 ];
